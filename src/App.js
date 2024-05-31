ee18997efda97bffaaab018460d70425eabe9e38
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-05-31 17:26:56 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import Textfade from "./components/Textfade";
+
+function App() {
+  return (
+    <div className="App">
+      <Textfade />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Textfade.js
@@ -0,0 +1,38 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
+import { useRef } from "react";
+
+function Textfade() {
+  const words = [
+    "GUGU!!!",
+    "GOOOODDDDDDDDDD",
+    "MORNINGGGGGGGG ☀️",
+    "🐹 💋💋💋",
+  ];
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 1500);
+
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <AnimatePresence mode="wait">
+      <motion.h1
+        key={words[index]}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        transition={{ duration: 0.5 }}
+        className="text-center font-display text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]"
+      >
+        {words[index]}
+      </motion.h1>
+    </AnimatePresence>
+  );
+}
+
+export default Textfade;
